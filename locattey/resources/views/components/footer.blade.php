b6eea67d201c7a233f533fb03a04f5a5f1a27c69
yukaritatikawa <tomosaki012@gmail.com>
2020-06-07 08:37:38 +0900

    laravelをバージョンアップにしたので古いバージョン分を削除

--- locattey/resources/views/components/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-    <p class="footer_text">&copy; ロケッティ All Rights Reserved.</p>
-</div>
-
-<script src="node_modules/jquery/dist/jquery.js"></script>
-<script src="src/app.js"></script>
-</body>
-</html>
